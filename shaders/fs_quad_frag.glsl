4a227e2dba9544879a3c982390ca3995957b9697
Tijmen Verhoef <tijmenmenno@gmail.com>
2023-12-14 18:16:30 +0100

    implemented shader compilation

--- shaders/fs_quad_frag.glsl
+++ shaders/fs_quad_frag.glsl
@@ -1,9 +1,11 @@
 #version 450
 #pragma shader_stage(fragment)
 
-out vec4 fragColor;
+layout(location = 0) in vec2 screenCoords;
+
+layout(location = 0) out vec4 fragColor;
 
 void main()
 {
-	fragColor = vec4(1, 1, 1, 1);
+	fragColor = vec4(screenCoords, 0, 1);
 }
--- shaders/fs_quad_vert.glsl
+++ shaders/fs_quad_vert.glsl
@@ -1,7 +1,10 @@
 #version 450
 #pragma shader_stage(vertex)
 
+layout(location = 0) out vec2 screenCoords;
+
 void main()
 {
-	//
+	screenCoords = vec2((gl_VertexIndex << 1) & 2, gl_VertexIndex & 2);
+    gl_Position = vec4(screenCoords * 2.0f + -1.0f, 0.0f, 1.0f);
 }
