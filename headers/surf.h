333d5864be1b2814de25bd463230135e162c83b1
Tijmen Verhoef <tijmenmenno@gmail.com>
2023-12-17 18:34:37 +0100

    fixed surface for unix systems

--- headers/surf.h
+++ headers/surf.h
@@ -8,5 +8,10 @@
 #define SCR_WIDTH		1280
 #define SCR_HEIGHT		720
 
+#if defined(_WIN32)
 #define MALLOC64(size)  _aligned_malloc(size, 64)
 #define FREE64(block)	_aligned_free(block)
+#elif defined(__unix)
+#define MALLOC64(size)  aligned_alloc(64, size)
+#define FREE64(block)	free(block)
+#endif
