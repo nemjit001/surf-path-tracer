b088c8c1670dfcbb83d078dbe98ec2585dadc30b
Tijmen Verhoef <tijmenmenno@gmail.com>
2023-12-17 15:21:45 +0100

    smoothed average frametime

--- sources/main.cpp
+++ sources/main.cpp
@@ -7,6 +7,9 @@
 
 #include "renderer.h"
 #include "timer.h"
+#include "types.h"
+
+#define NUM_SMOOTH_FRAMES 20	// Number of frames to smooth FPS / frame timing over
 
 int main()
 {
@@ -27,9 +30,19 @@ int main()
 
 	while (!glfwWindowShouldClose(window))
 	{
+		static F32 SMOOTH_FRAC = 1.0f / NUM_SMOOTH_FRAMES;
+		static F32 AVERAGE_FRAMETIME = 10.0f;
+		static F32 ALPHA = 1.0f;
+
 		frameTimer.tick();
 		F32 deltaTime  = frameTimer.deltaTime();
-    	printf("%08.2fms (%08.2ffps)\n", deltaTime * 1'000.0f, 1.0f / deltaTime);
+
+		AVERAGE_FRAMETIME = (1.0f - ALPHA) * AVERAGE_FRAMETIME + ALPHA * (deltaTime * 1'000.0f);
+		if (ALPHA > SMOOTH_FRAC)
+			ALPHA *= 0.5;
+
+		F32 inv_avg_frametime = 1.0f / AVERAGE_FRAMETIME;
+    	printf("%08.2fms (%08.2ffps)\n", AVERAGE_FRAMETIME, inv_avg_frametime * 1'000.0f);
 
 		renderer.render(deltaTime);
 		glfwPollEvents();
